f5f061ed545e795c4951a15a1200090980d1dac7
Christian Ashby <git@cashby.me.uk>
2019-08-11 08:03:01 +0100

    Adding debounce functionality

--- esp32-showcontrol.ino
+++ esp32-showcontrol.ino
@@ -10,12 +10,14 @@ struct LED {
 
 struct Button {
     const uint8_t PIN;
-    uint32_t numberKeyPresses;
+    long pressCount;
     bool pressed;
+    unsigned long debounceMillis;
     LED* led;
 };
 
 const long LED_TIME = 1000;
+const long DEBOUNCE_TIME = 100;
 #define COUNT 4
 
 // PIN, state, lastChangeMillis
@@ -23,11 +25,11 @@ LED go_led = { 33, false, -1 };
 LED stop_led = { 15, false, -1 };
 LED prev_led = { 4, false, -1 };
 LED next_led = { 13, false, -1 };
-// PIN, numberKeyPresses, pressed, led
-Button go_button = {34, 0, false, &go_led};
-Button stop_button = {35, 0, false, &stop_led};
-Button prev_button = {36, 0, false, &prev_led};
-Button next_button = {39, 0, false, &next_led};
+// PIN, pressCount, pressed, debounceMillis, led
+Button go_button = {34, 0, false, -1, &go_led};
+Button stop_button = {35, 0, false, -1, &stop_led};
+Button prev_button = {36, 0, false, -1, &prev_led};
+Button next_button = {39, 0, false, -1, &next_led};
 
 char *names[] = { "go", "stop", "prev", "next" };
 LED* leds[]= { &go_led, &stop_led, &prev_led, &next_led };
@@ -40,11 +42,15 @@ SSD_13XX tft = SSD_13XX(__CS, __DC, __RST);
 
 void IRAM_ATTR isr(void* arg) {
     Button* s = static_cast<Button*>(arg);
-    s->numberKeyPresses += 1;
-    s->pressed = true;
-    s->led->state = true;
-    s->led->lastChangeMillis = millis();
-    digitalWrite(s->led->PIN, !s->led->state);
+    unsigned long now = millis();
+    if( (now - s->debounceMillis) >= DEBOUNCE_TIME ) {
+      s->debounceMillis = millis();
+      s->pressCount++;
+      s->pressed = true;
+      s->led->state = true;
+      s->led->lastChangeMillis = millis();
+      digitalWrite(s->led->PIN, !s->led->state);
+    }
 }
 
 void setup() {
@@ -64,18 +70,17 @@ void setup() {
 }
 
 void loop() {  
+    unsigned long now = millis();
     for( int c = 0; c < COUNT; c++ ) {
         if( buttons[c]->pressed ) {
-          Serial.printf("%s has been pressed %u times\n", names[c], buttons[c]->numberKeyPresses);
+          Serial.printf("%s has been pressed %u times\n", names[c], buttons[c]->pressCount);
           tft.clearScreen();
           tft.setCursor(0, 0);
           tft.setTextColor(WHITE);
           tft.setTextScale(1);
           tft.println(names[c]);
           buttons[c]->pressed = false;
-          // TODO: Debounce me!
         }
-        unsigned long now = millis();
         if( leds[c]->state && (now - leds[c]->lastChangeMillis) >= LED_TIME) {
           leds[c]->state = false;
           digitalWrite(leds[c]->PIN, !leds[c]->state);
